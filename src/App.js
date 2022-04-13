3dbb6208d6dce2c8a81958efe29a738ad83c791c
Oleg <swigerd2l@gmail.com>
2022-04-13 20:15:21 +0300

    mne poxuy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MainNavbar from './components/MainNavbar';
 import MainContent from './components/MainContent';
 import LoadingPage from './components/LoadingPage';
 import { Routes } from 'react-router-dom';
-
+// нахуя
 function App() {
   return (
     <>
